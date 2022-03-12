aa3e01eabb8945be3267dbca2e8678fd682544b6
thenaijagamer <thenaijagamer>
2022-03-12 16:10:17 +0100

    made a small change to the quiz app

--- quiz/app.js
+++ quiz/app.js
@@ -1,6 +1,6 @@
 const form = document.querySelector('form');
 const header = document.querySelector('#header');
-const correctAnswers = ['a', 'b', 'b', 'a'];
+const correctAnswers = ['a', 'b', 'b', 'a', 'b', 'a', 'a', 'b', 'a', 'b'];
 const para = document.createElement('p');
 para.classList.add('score-value')
 header.appendChild(para);
@@ -9,12 +9,12 @@ header.appendChild(para);
 form.addEventListener('submit', e => {
   e.preventDefault();
   let score = 0;
-  const userAnswers = [form.q1.value, form.q2.value, form.q3.value, form.q4.value ]
+  const userAnswers = [form.q1.value, form.q2.value, form.q3.value, form.q4.value,form.q5.value, form.q6.value, form.q7.value, form.q8.value, form.q9.value,form.q10.value ]
   
   // check answers
   userAnswers.forEach((answer, index) => {
     if (answer === correctAnswers[index]){
-      score +=25;
+      score +=10;
     }
   })
 
